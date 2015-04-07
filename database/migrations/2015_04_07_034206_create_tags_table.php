dfbea47b350bb18254361df6a79634783831aeeb
BillyAndrys <billyandrys@gmail.com>
2015-04-06 23:11:48 -0500

    agregado dos campos en la migracion tags

--- database/migrations/2015_04_07_034206_create_tags_table.php
+++ database/migrations/2015_04_07_034206_create_tags_table.php
@@ -15,6 +15,8 @@ class CreateTagsTable extends Migration {
 		Schema::create('tags', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('name',20);
+			$table->mediumText('description')->nullable();
 			$table->timestamps();
 		});
 	}
