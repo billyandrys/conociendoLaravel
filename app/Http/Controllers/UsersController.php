d0fdb91eb3a5a06267c730d46c8cad46f58abe56
BillyAndrys <billyandrys@gmail.com>
2015-04-08 22:38:16 -0500

    creado metodo orm

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,6 +1,17 @@
 <?php namespace coursee\Http\Controllers;
 
+use coursee\User;
+
 class UsersController extends Controller {
+
+
+	public function getOrm()
+	{
+		$result = User::get();
+		dd($result->profile->age);
+
+	}
+
 
 	public function getIndex()
 	{
@@ -16,7 +27,7 @@ class UsersController extends Controller {
 		->get();
 
 		//dd($resul->first_name);
-		return $resul;
+		//return $resul;
 	}
 
 }	
